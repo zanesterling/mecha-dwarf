50be10155d86eb6c87d5384de556b7684250bda7
Zane Sterling <sterling.zane@gmail.com>
2024-01-12 10:31:16 -0500

    Macho header from() and show()

---
+++ src/macho.rs
@@ -0,0 +1,39 @@
+use std::mem::transmute;
+
+#[derive(Debug)]
+pub struct Header {
+    magic: u32,
+    cpu_type: u32,
+    cpu_subtype: u32,
+    file_type: u32,
+    loads_count: u32,
+    loads_size: u32,
+    flags: u32,
+    reserved: u32,
+}
+
+impl Header {
+    pub fn from(bytes: [u8; 32]) -> Header {
+        unsafe {
+            transmute::<[u8; 32], Header>(bytes)
+        }
+    }
+
+    pub fn show(&self) {
+        println!("Header {{");
+        println!("\tmagic:       {:#010x}", self.magic);
+        println!("\tcpu_type:    {:#010x}", self.cpu_type);
+        println!("\tcpu_subtype: {:#010x}", self.cpu_subtype);
+        println!("\tfile_type:   {:#010x}", self.file_type);
+        println!("\tloads_count: {:#010x}", self.loads_count);
+        println!("\tloads_size:  {:#010x}", self.loads_size);
+        println!("\tflags:       {:#010x}", self.flags);
+        println!("\treserved:    {:#010x}", self.reserved);
+        println!("}}");
+    }
+}
+
+#[derive(Debug)]
+pub struct File {
+    header: Header,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod macho;
+
 fn main() {
     let path = {
         let mut args: Vec<String> = std::env::args().collect();
@@ -9,6 +11,13 @@ fn main() {
     };
 
     println!("loading from file: {}", path);
+    let mut bytes: [u8; 32] = [0; 32];
+    bytes[0] = 0xcf;
+    bytes[1] = 0xfa;
+    bytes[2] = 0xed;
+    bytes[3] = 0xfe;
+    let header = macho::Header::from(bytes);
+    header.show()
 }
 
 fn usage(args: Vec<String>) {
