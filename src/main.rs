582aad8ccc9380f94b4d2001b661f17153778b07
Zane Sterling <sterling.zane@gmail.com>
2024-01-12 10:18:25 -0500

    Argv handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let path = {
+        let mut args: Vec<String> = std::env::args().collect();
+        if args.len() != 2 {
+            usage(args);
+            std::process::exit(1);
+        }
+        args.swap_remove(1)
+    };
+
+    println!("loading from file: {}", path);
+}
+
+fn usage(args: Vec<String>) {
+    println!("usage: {} FILENAME", args[0]);
 }
