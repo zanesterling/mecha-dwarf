19c0f6b664ec70532dd44e55415b652a606c7491
Zane Sterling <sterling.zane@gmail.com>
2024-01-26 12:25:09 +0100

    Parse verbose flag

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,30 @@ use memmap::{ Mmap, MmapOptions };
 
 mod macho;
 
-fn main() {
-    let path = {
-        let mut args: Vec<String> = std::env::args().collect();
-        if args.len() != 2 {
-            usage(args);
-            std::process::exit(1);
-        }
-        args.swap_remove(1)
-    };
+struct Config {
+    path: String,
+    verbose: bool,
+}
 
-    let mmap: Mmap = mmap_file(path)
+fn main() {
+    let config = parse_config(std::env::args());
+    let mmap: Mmap = mmap_file(config.path)
         .unwrap_or_else(|e| {
             println!("{}", e);
             std::process::exit(1);
         });
+
+    // Parse the Mach-O file.
     let macho = macho::File::from(&mmap[..])
         .unwrap_or_else(|e| {
             println!("error parsing macho: {}", e);
             std::process::exit(1);
         });
+    if config.verbose {
+        println!("{:#x?}", macho);
+    }
 
+    // Get the DWARF segment
     let mut dwarf: Option<macho::Segment64> = None;
     for cmd in macho.load_commands {
         if let macho::LoadCommandDetails::Segment64(seg) = cmd.details {
@@ -42,7 +45,28 @@ fn main() {
 }
 
 fn usage(args: Vec<String>) {
-    println!("usage: {} FILENAME", args[0]);
+    println!("usage: {} [-v] FILENAME", args[0]);
+}
+
+fn parse_config(args: std::env::Args) -> Config {
+    let mut args: Vec<String> = args.collect();
+    let mut config = Config {
+        path: String::from(""),
+        verbose: false,
+    };
+    for i in 1..args.len() {
+        if args[i] == "-v" {
+            config.verbose = true;
+            args.swap_remove(i);
+            break;
+        }
+    }
+    if args.len() != 2 {
+        usage(args);
+        std::process::exit(1);
+    }
+    config.path = args.swap_remove(1);
+    config
 }
 
 fn mmap_file(path: String) -> Result<Mmap, String> {
